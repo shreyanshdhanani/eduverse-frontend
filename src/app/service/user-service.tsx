12489050a2baf8e097d5735ec67b297fff3b3a3a
vinit koladiya <88574441+Utsavvasani@users.noreply.github.com>
2026-04-08 02:48:09 +0530

    feat: implement getAllUser service to fetch student data with error handling

--- src/app/service/user-service.tsx
+++ src/app/service/user-service.tsx
@@ -1,19 +1,16 @@
 import { AxiosError } from 'axios';
-import axios from 'axios';
+import axiosInstance from './axiosInstance';
 
-const API_URL = process.env.API_URL || 'http://localhost:3020/api';
-
-export const getAllUser= async()=>{
-    try{
-        const response = await axios.get(`${API_URL}/student`);
-        return response.data; 
-    }catch(error: unknown)
-    {
-        if (error instanceof AxiosError) {
-            const errorMessage = error.response?.data?.message || 'Registration failed. Please try again.';
-            throw new Error(errorMessage);
-          } else {
-            throw new Error('An unexpected error occurred. Please try again later.');
-          }
+export const getAllUser = async () => {
+  try {
+    const response = await axiosInstance.get(`/student`);
+    return response; 
+  } catch (error: unknown) {
+    if (error instanceof AxiosError) {
+      const errorMessage = error.response?.data?.message || 'Failed to fetch users. Please try again.';
+      throw new Error(errorMessage);
+    } else {
+      throw new Error('An unexpected error occurred. Please try again later.');
     }
-}
+  }
+};
